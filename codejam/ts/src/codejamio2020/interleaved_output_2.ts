69d807f9f5912e29d2173736e7e35d46143cc68a
lisamai <l.maihuynh@gmail.com>
2021-01-12 21:55:56 -0500

    a mind of its own

--- codejam/ts/src/codejamio2020/interleaved_output_2.ts
+++ codejam/ts/src/codejamio2020/interleaved_output_2.ts
@@ -65,62 +65,123 @@ async function parse(inputStream: StdinLineStream): Promise<[string]> {
   return [await inputStream.getLine()]
 }
 
+// const MACHINE_INDEX = {
+//   IO: 0b1000,
+//   Io: 0b100,
+//   iO: 0b10,
+//   io: 0b1,
+// }
+
+// function sol(input: string): string {
+//   const dp = new Array(input.length + 1)
+//     .fill(null)
+//     .map(() => new Array(16).fill(-1));
+//   dp[0][0] = 0;
+
+//   const n = input.length;
+//   for (let i = 0; i < n; i++) {
+//     const c = input.charAt(i);
+
+//     for (let j = 0; j < 16; j++) {
+//       // Invalid state
+//       if (dp[i][j] < 0) {
+//         continue;
+//       }
+
+//       if (c === 'I' || c === 'i') {
+//         const checkMachines = c === 'I' ?
+//           [MACHINE_INDEX.IO, MACHINE_INDEX.Io] :
+//           [MACHINE_INDEX.io, MACHINE_INDEX.iO];
+
+//         checkMachines.forEach((machineIndex) => {
+//           // If machine ain't started, start
+//           if (!(machineIndex & j)) {
+//             dp[i + 1][j | machineIndex] = Math.max(dp[i + 1][j | machineIndex], dp[i][j])
+//           }
+//         });
+//       }
+
+//       if (c === 'O' || c === 'o') {
+//         const checkMachines = c === 'O' ?
+//           [MACHINE_INDEX.IO, MACHINE_INDEX.iO] :
+//           [MACHINE_INDEX.Io, MACHINE_INDEX.io];
+
+//         checkMachines.forEach((machineIndex) => {
+//           // If machine started, finish
+//           if (machineIndex & j) {
+//             const score = machineIndex === MACHINE_INDEX.IO ? 1 : 0;
+//             dp[i + 1][j ^ machineIndex] = Math.max(dp[i + 1][j ^ machineIndex], dp[i][j] + score)
+//           }
+//         });
+//       }
+//     }
+//   }
+
+//   const total = dp[n][0];
+//   return `${total}`;
+// }
+
+
 const MACHINE_INDEX = {
-  IO: 0b1000,
-  Io: 0b100,
-  iO: 0b10,
-  io: 0b1,
+  IO: 1<<0,
+  io: 1<<1,
+  Io: 1<<2,
+  iO: 1<<3
 }
 
+let broadcast: string;
+let cache: number[][];
+
 function sol(input: string): string {
-  const dp = new Array(input.length + 1)
+  broadcast = input;
+  cache = new Array(input.length)
     .fill(null)
-    .map(() => new Array(16).fill(-1));
-  dp[0][0] = 0;
+    .map(() => new Array(16).fill(-1))
 
-  const n = input.length;
-  for (let i = 0; i < n; i++) {
-    const c = input.charAt(i);
-
-    for (let j = 0; j < 16; j++) {
-      // Invalid state
-      if (dp[i][j] < 0) {
-        continue;
-      }
-
-      if (c === 'I' || c === 'i') {
-        const checkMachines = c === 'I' ?
-          [MACHINE_INDEX.IO, MACHINE_INDEX.Io] :
-          [MACHINE_INDEX.io, MACHINE_INDEX.iO];
-
-        checkMachines.forEach((machineIndex) => {
-          // If machine ain't started, start
-          if (!(machineIndex & j)) {
-            dp[i + 1][j | machineIndex] = Math.max(dp[i + 1][j | machineIndex], dp[i][j])
-          }
-        });
-      }
-
-      if (c === 'O' || c === 'o') {
-        const checkMachines = c === 'O' ?
-          [MACHINE_INDEX.IO, MACHINE_INDEX.iO] :
-          [MACHINE_INDEX.Io, MACHINE_INDEX.io];
-
-        checkMachines.forEach((machineIndex) => {
-          // If machine started, finish
-          if (machineIndex & j) {
-            const score = machineIndex === MACHINE_INDEX.IO ? 1 : 0;
-            dp[i + 1][j ^ machineIndex] = Math.max(dp[i + 1][j ^ machineIndex], dp[i][j] + score)
-          }
-        });
-      }
-    }
-  }
-
-  const total = dp[n][0];
+  const total = calc(0, 0);
   return `${total}`;
 }
 
+function calc(index: number, machineState: number): number {
+  if (index === broadcast.length) {
+    return 0;
+  }
+
+  if (cache[index][machineState] > -1) {
+    return cache[index][machineState];
+  }
+
+  const c = broadcast.charAt(index);
+  let counts = [];
+  if (c === 'I' || c === 'i') {
+    const machines = c === 'I' ?
+      [MACHINE_INDEX.IO, MACHINE_INDEX.Io] : [MACHINE_INDEX.io, MACHINE_INDEX.iO];
+    machines.forEach((machine_index) => {
+      if (!(machineState & machine_index)) {
+        counts.push(calc(index + 1, machineState | machine_index));
+      }
+    })
+  }
+
+  if (c === 'O' || c === 'o') {
+    const machines = c === 'O' ?
+      [MACHINE_INDEX.IO, MACHINE_INDEX.iO] : [MACHINE_INDEX.io, MACHINE_INDEX.Io];
+    machines.forEach((machine_index) => {
+      if (machineState & machine_index) {
+        let updateCount = machine_index === MACHINE_INDEX.IO ? 1 : 0;
+        counts.push(calc(index + 1, machineState ^ machine_index) + updateCount);
+      }
+    })
+  }
+
+  const result = Math.max.apply(null, counts);
+  cache[index][machineState] = result;
+  return result;
+}
+
+/**
+ * old jank
+ */
 // function calc(index: number, input: string, machineState: MachineState, currCount: number): number[] {
 //   if (index === input.length) {
 //     return [currCount];
